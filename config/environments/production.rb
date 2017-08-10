525d7396183c208c7b68b4f4850a7cf3fe40b639
amsterdamAL <amsterdamAL@gmail.com>
2017-08-10 03:08:45 -0400

    get it going

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,8 +8,12 @@ Rails.application.configure do
     :bucket => ENV['S3_BUCKET_NAME'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
   }
-}
+
+
+    
+config.secret_key_base = ENV["SECRET_KEY_BASE"]
 
 
 

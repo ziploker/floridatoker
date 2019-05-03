9519c3c1ec100eff9d01e902215cc7bb93a06340
amsterdamAL <amsterdamAL@gmail.com>
2019-05-03 01:25:45 -0400

    heroku fix

--- app/controllers/lookups_controller.rb
+++ app/controllers/lookups_controller.rb
@@ -17,25 +17,24 @@ require 'pry'
 	end
 
 # For all responses in this controller, return the CORS access control headers.
-
-#def cors_set_access_control_headers
-#  headers['Access-Control-Allow-Origin'] = '*'
-#  headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-#  headers['Access-Control-Request-Method'] = '*'
-#  headers['Access-Control-Allow-Headers'] = '*'
-#  headers['Access-Control-Max-Age'] = "1728000"
-#end
+def cors_set_access_control_headers
+  headers['Access-Control-Allow-Origin'] = '*'
+  headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+  headers['Access-Control-Request-Method'] = '*'
+  headers['Access-Control-Allow-Headers'] = '*'
+  headers['Access-Control-Max-Age'] = "1728000"
+end
 #  
-#  def cors_preflight_check
-#  if request.method == :options
-#    headers['Access-Control-Allow-Origin'] = '*'
-#    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-#    headers['Access-Control-Allow-Headers'] = '*'
-#    headers['Access-Control-Request-Method'] = '*'
-#    headers['Access-Control-Max-Age'] = '1728000'
-#    render :text => '', :content_type => 'text/plain'
-#  end
-#end
+  def cors_preflight_check
+  if request.method == :options
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = '*'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Max-Age'] = '1728000'
+    render :text => '', :content_type => 'text/plain'
+  end
+end
   def new
     
   end

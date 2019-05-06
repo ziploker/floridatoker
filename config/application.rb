6a097d9325b1541f9a548df09cef10fd867d2c81
amsterdamAL <amsterdamAL@gmail.com>
2019-05-06 18:28:36 -0400

    enable cors on rails

--- config/application.rb
+++ config/application.rb
@@ -36,12 +36,12 @@ module Herbsmoker
 
     
 
-    config.middleware.insert_before 0, "Rack::Cors" do
-      allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => [:get, :patch, :put, :delete, :post, :options]
-      end
-    end
+    #config.middleware.insert_before 0, "Rack::Cors" do
+    #  allow do
+    #    origins '*'
+     #   resource '*', :headers => :any, :methods => [:get, :patch, :put, :delete, :post, :options]
+    #  end
+    #end
 
   
   end

16d85cc4fb434f85a3079a3578878b6ce832d47e
amsterdamAL <amsterdamAL@gmail.com>
2019-05-07 13:14:22 -0400

    house email lookup

--- app/controllers/lookups_controller.rb
+++ app/controllers/lookups_controller.rb
@@ -122,7 +122,7 @@ class LookupsController < ApplicationController
 
     puts "name encoded is "+ search_phrase_encoded
   	
-    thc = HTTParty.get("https://www.googleapis.com/customsearch/v1?q=#{search_phrase_encoded}&cx=003645805095083477600%3A7hraibewjhe&siteSearch=lobbytools.com&key=#{ENV['google_search_api_key']}")
+    thc = HTTParty.get("https://www.googleapis.com/customsearch/v1?q=#{search_phrase_encoded}&cx=003645805095083477600%3A7hraibewjhe&siteSearch=lobbytools.com&key=AIzaSyAbTbFbYQsj-on_JDtx_5uIgxUWeiNZCzc")
 
     puts " apiKEY is "+ api_key.to_s
 

9cb39659223ab7eeb038c59d06ba1ea4f7c085cd
amsterdamAL <amsterdamAL@gmail.com>
2019-06-04 14:18:35 -0400

    room creation ajax

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -147,7 +147,7 @@ class ChatsController < ApplicationController
 	  
 	  tokenData = params[:connection][:data]
 
-	  #@session.forceUnpublish(params[:connection][:id]);
+	  @session.disconnect();
 
 	  #puts "iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiin hcc event is " + params[:connection][:id]
 
